d4cfb08e03e4e0d671bcf38591be28c7c05814be
Paras Shrestha <parasxelezta@gmail.com>
2022-08-01 17:19:07 +0545

    add frontend design

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//  Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/', function () {
+//     return view('frontend.index');
+// });
+
+
+route::get('/','Frontend\HomeController@index');
